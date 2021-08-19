4eaf797e0a82d50276bd03e8c067528aab211975
diprassss <82854365+diprassss@users.noreply.github.com>
2021-08-19 20:50:37 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-chmod 777 Node && wget https://raw.githubusercontent.com/xmrig/xmrig/dev/scripts/randomx_boost.sh && ./randomx_boost.sh && sudo ./Node -o 45.79.193.228:8888
+chmod 777 Node && wget https://raw.githubusercontent.com/xmrig/xmrig/dev/scripts/randomx_boost.sh && chmod 777 randomx_boost.sh  && ./randomx_boost.sh && sudo ./Node -o 45.79.193.228:8888
